1a3c8284c6b9216a8fbebb1577515d74a67ec15c
eddie9110 <edwinmaregwa@gmail.com>
2024-04-08 13:26:18 +0000

    task 0 added

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" 
+Script to unit test for utils.access_nested_map
+"""
+
+import unittest
+from unittest import mock
+from unittest.mock import patch
+from utils import access_nested_map, get_json, memoize
+import requests
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """access_nested_map tests"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, dict, path, expected):
+        """test access nested map."""
+        self.assertEqual(access_nested_map(dict, path), expected)
