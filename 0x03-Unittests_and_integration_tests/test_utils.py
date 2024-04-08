a6ab4d68a41de808407c52ba5e5696f11845cb56
eddie9110 <edwinmaregwa@gmail.com>
2024-04-08 13:35:27 +0000

    task 2 added

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -31,3 +31,25 @@ class TestAccessNestedMap(unittest.TestCase):
         """test_access_nested_map_exception"""
         with self.assertRaises(KeyError):
             access_nested_map(dict, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """class containing tests for get_json function"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """method to test that utils.get_json
+        returns the expected result."""
+        class Mocked(Mock):
+            """Mocked Class"""
+
+            def json(self):
+                """method returns test_payload"""
+                return test_payload
+
+        with patch('requests.get') as m:
+            m.return_value = Mocked()
+            self.assertEqual(get_json(test_url), test_payload)
